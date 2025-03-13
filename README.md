bf5ffb3ea3fd809db7b453a464b52492fda025bc
Anton Dambrouski <Anton.Dambrouski@ventionteams.com>
2025-03-13 19:51:15 +0100

    Add Task 1

---
+++ README.md
@@ -0,0 +1,3 @@
+Чтобы склонировать репозиторий - используй команду git clone `https://github.com/AntonDambrouski/advanced-aspnet-course.git`
+
+Далее ты найдешь описание заданий в каждой из папок. Удачи!
---
+++ Task 1/Debugging/Program.cs
@@ -0,0 +1,74 @@
+﻿const int NumsCount = 10;
+int[] nums = new int[NumsCount];
+// rand initializations
+Input(nums);
+
+//sort
+SelectionSort(nums);
+
+if (!CheckSortResults(nums))
+{
+  Console.WriteLine("Array is unsorted!");
+}
+
+// output
+Console.WriteLine("Array:");
+Output(nums);
+
+bool CheckSortResults(int[] nums)
+{
+  for (int i = 1; i < nums.Length; i++)
+  {
+    if (nums[i - 1] > nums[i])
+    {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+void Output(int[] nums)
+{
+  for (int i = 0; i < nums.Length; i++)
+  {
+    Console.WriteLine(nums[i]);
+  }
+}
+
+void Input(int[] nums)
+{
+  Random random = new Random();
+  for (int i = 0; i < nums.Length; i++)
+  {
+    nums[i] = random.Next(0, 100);
+  }
+}
+
+void SelectionSort(int[] nums)
+{
+  for (var i = nums.Length - 1; i > 0; i--)
+  {
+    var maxIndex = i;
+    int j;
+    for (j = i - 1; j > 0; --j)
+    {
+      if (nums[j] > nums[maxIndex])
+      {
+        maxIndex = j;
+      }
+    }
+
+    if (i != maxIndex)
+    {
+      Swap(ref nums[i], ref nums[maxIndex]);
+    }
+  }
+}
+
+void Swap(ref int x, ref int y)
+{
+  var t = x;
+  x = y;
+  y = t;
+}
