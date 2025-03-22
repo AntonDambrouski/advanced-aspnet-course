1f8ba3d8f8f3626642058308e25645e7b63f169f
alexdesty <alex.desty@gmail.com>
2025-03-22 22:13:18 +0300

    Reapply "Add mvc app"
    This reverts commit 101a7b3fbc5cd021f961afd06ce2da4d7d689e97.

---
+++ Lessons/MVCBasics/FirstMvcApp/FirstMvcApp/Services/ProductService.cs
@@ -0,0 +1,50 @@
+﻿using FirstMvcApp.Models;
+
+namespace FirstMvcApp.Services;
+
+public class ProductService
+{
+    private static readonly List<Product> _products = new List<Product>();
+
+    static ProductService()
+    {
+        _products.Add(new Product { Id = 1, Name = "Keyboard", Price = 20 });
+        _products.Add(new Product { Id = 2, Name = "Mouse", Price = 10 });
+        _products.Add(new Product { Id = 3, Name = "Monitor", Price = 100 });
+    }
+
+    public List<Product> GetAllProducts()
+    {
+        return _products;
+    }
+
+    public Product? GetProductById(int id)
+    {
+        return _products.FirstOrDefault(p => p.Id == id);
+    }
+
+    public void CreateProduct(Product product)
+    {
+        product.Id = _products.Max(p => p.Id) + 1;
+        _products.Add(product);
+    }
+
+    public void UpdateProduct(int id, Product product)
+    {
+        var existingProduct = _products.FirstOrDefault(p => p.Id == id);
+        if (existingProduct != null)
+        {
+            existingProduct.Name = product.Name;
+            existingProduct.Price = product.Price;
+        }
+    }
+
+    public void DeleteProduct(int id)
+    {
+        var product = _products.FirstOrDefault(p => p.Id == id);
+        if (product != null)
+        {
+            _products.Remove(product);
+        }
+    }
+}
