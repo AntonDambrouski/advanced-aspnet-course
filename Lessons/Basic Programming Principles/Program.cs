eab3288b6a887ff7820062d80d0a8513c391e809
alexdesty <alex.desty@gmail.com>
2025-03-22 22:58:02 +0300

    Edit one file

--- Lessons/Basic Programming Principles/Program.cs
+++ Lessons/Basic Programming Principles/Program.cs
@@ -1,10 +1,5 @@
 ﻿
 using Basic_Programming_Principles.DesignPatterns;
-<<<<<<< HEAD
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 StateDemo.Execute();
-=======
-
-StrategyDemo.Execute();
->>>>>>> parent of 3792472 (Fixed the bug)
