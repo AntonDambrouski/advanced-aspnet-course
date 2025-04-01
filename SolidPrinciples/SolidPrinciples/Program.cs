1900f4ffe75112d881f0a9500f918447d5d258ec
alexdesty <alex.desty.one@gmail.com>
2025-04-01 16:58:55 +0300

    Add one solid principle

---
+++ SolidPrinciples/SolidPrinciples/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SolidPrinciples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SingleResponsibilityProblem singleResposibilityProblem = new SingleResponsibilityProblem();
+            singleResposibilityProblem.Execute();
+            SingleResponsibilitySolution singleResposibilitySolution = new SingleResponsibilitySolution();
+            singleResposibilitySolution.Execute();
+        }
+    }
+}
---
+++ SolidPrinciples/SolidPrinciples/SingleResponsibilitySolution.cs
@@ -0,0 +1,44 @@
+﻿namespace SolidPrinciples;
+
+internal class SingleResponsibilitySolution
+{
+    public class Document
+    {
+        public string Name { get; set; }
+    }
+
+    public class UploaderAndDownloader
+    {
+        public void UploadDocument(Document document)
+        {
+            Console.WriteLine($"Uploading {document.Name}");
+        }
+
+        public void DownloadDocument(Document document)
+        {
+            Console.WriteLine($"Downloading {document.Name}");
+        }
+    }
+
+    public class Printer
+    {
+        public void PrintDocument(Document document)
+        {
+            Console.WriteLine($"Printing {document.Name}");
+        }
+    }
+
+    public void Execute()
+    {
+        Console.WriteLine("\nSingle responsibility solution");
+        Document document = new Document()
+        {
+            Name = "Report"
+        };
+        UploaderAndDownloader uploaderAndDownloader = new UploaderAndDownloader();
+        Printer printer = new Printer();
+        uploaderAndDownloader.UploadDocument(document);
+        uploaderAndDownloader.DownloadDocument(document);
+        printer.PrintDocument(document);
+    }
+}
