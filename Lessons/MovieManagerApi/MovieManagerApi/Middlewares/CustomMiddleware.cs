154e1271e2efbb98cacfe6b05e54d8d2c788001c
Anton Dambrouski <Anton.Dambrouski@ventionteams.com>
2025-04-03 19:42:41 +0200

    Initialize MovieManagerApi with CRUD operations
    This commit introduces a new ASP.NET Core Web API project named `MovieManagerApi`. It includes the implementation of CRUD operations for `Movie` and `Review` entities, along with necessary configurations in the solution and project files.
    
    Key changes:
    - Created solution file (`MovieManagerApi.sln`) and project file (`MovieManagerApi.csproj`) targeting .NET 8.0.
    - Added configuration files for logging and allowed hosts.
    - Implemented controllers for handling API requests and service interfaces for business logic.
    - Introduced middleware for exception handling and a custom filter for response headers.
    - Added documentation in `README.md` and HTTP request definitions for testing.
    - Configured launch settings for different environments.
    
    These changes establish a foundational structure for the web API application.

---
+++ Lessons/MovieManagerApi/MovieManagerApi/Middlewares/CustomMiddleware.cs
@@ -0,0 +1,17 @@
+﻿namespace MovieManagerApi.Middlewares;
+
+public class CustomMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public CustomMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        Console.WriteLine("Hello from Custom Middleware");
+       // await _next(context);
+    }
+}
