20a32d6ed188971605ec76c0aaed5d00648431c6
Nick <byklin@list.ru>
2025-04-04 22:52:30 +0300

    Nicks Task 4 NicksSchoolDiaryApi

---
+++ Tasks/Task 4/NicksSchoolDiaryApi/Filters/CheckClassIdFilterAttribute.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using NicksSchoolDiaryApi.Middleware;
+using NicksSchoolDiaryApi.Models;
+using NicksSchoolDiaryApi.Services;
+
+namespace NicksSchoolDiaryApi.Filters
+{
+    public class CheckClassIdFilterAttribute : Attribute, IAsyncActionFilter
+    {
+        private readonly IStudentClassService _studentClassService;
+        public CheckClassIdFilterAttribute(IStudentClassService studentClassService)
+        {
+            _studentClassService = studentClassService;
+        }   
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+
+            if (context.ActionArguments.ContainsKey("student"))
+            {
+                var student = context.ActionArguments["student"];
+                if (student is Student studentObj)
+                {
+                    if ( _studentClassService.GetClass(studentObj.StudentClassId) is null )
+                    {
+                        studentObj.StudentClassId = _studentClassService.GetStudentClasses().First().Id;
+                        if (studentObj.StudentClassId <= 0)
+                        {
+                            context.Result = new BadRequestObjectResult("Class id must be greater than 0");
+                            return;
+                        }
+                        context.ActionArguments["student"] = studentObj;
+                    }
+                }               
+            }
+            else
+            {
+                context.Result = new BadRequestObjectResult("Student is required");
+                return;
+            }
+            await next();
+        }
+    }
+
+
+   
+
+}
---
+++ Tasks/Task 4/NicksSchoolDiaryApi/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using NicksSchoolDiaryApi.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace NicksSchoolDiaryApi.Models
+{
+
+    
+    public class Student
+    {
+      
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(45, MinimumLength = 3)]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        [DateOfBirth]
+        public DateOnly DateOfBirth { get; set; }
+
+        [Required]
+        public int StudentClassId { get; set; }
+    }
+}
