12c068c65bc3c47763709d82896492fd555888ac
Anton DAMBROUSKI <anton.dambrouski@veepee.com>
2025-07-29 16:32:05 +0200

    Update Kafka projects for .NET 9.0 and add Docker support
    - Updated KafkaConsumer and KafkaProducer project files to target .NET 9.0 and include Confluent.Kafka package version 2.11.0.
    - Modified Program.cs in both projects to implement Kafka consumer and producer functionality with proper exception handling.
    - Added docker-compose.yml to define a Kafka broker service.
    - Created a new solution file (KafkaProducer.sln) for better project management in Visual Studio.

---
+++ Lessons/Kafka/KafkaConsumer/Program.cs
@@ -0,0 +1,30 @@
+﻿using Confluent.Kafka;
+
+var groupId = args.First();
+var consumerConfig = new ConsumerConfig
+{
+    BootstrapServers = "localhost:9092",
+    GroupId = groupId,
+    AutoOffsetReset = AutoOffsetReset.Earliest,
+    
+};
+
+using var consumer = new ConsumerBuilder<Ignore, string>(consumerConfig).Build();
+var topic = "test-topic";
+
+consumer.Subscribe(topic);
+
+Console.WriteLine($"Subscribed to topic '{topic}'. Waiting for messages...");
+while (true)
+{
+    try
+    {
+        var consumedMessage = consumer.Consume();
+        await Task.Delay(500);
+        Console.WriteLine($"Consumed message '{consumedMessage.Message.Value}' at offset {consumedMessage.Offset} from topic '{consumedMessage.Topic}'.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Unable to consume message. Reason: {ex.Message}");
+    }
+}
---
+++ Lessons/Kafka/KafkaProducer/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using Confluent.Kafka;
+
+var producerConfig = new ProducerConfig
+{
+    BootstrapServers = "localhost:9092",
+	SecurityProtocol = SecurityProtocol.Plaintext,
+};
+
+using var producer = new ProducerBuilder<Null, string>(producerConfig).Build();
+var topic = "test-topic";
+
+while (true)
+{
+    var message = Console.ReadLine();
+    if (string.IsNullOrEmpty(message))
+    {
+        return;
+    }
+
+	try
+	{
+		var messageToSend = new Message<Null, string>
+		{
+			Value = message,
+		};
+
+		var result = producer.ProduceAsync(topic, messageToSend);
+		Console.WriteLine($"Message '{message}' sent to topic '{topic}' at offset {result.Result.Offset}.");
+    }
+	catch (Exception ex)
+	{
+
+        Console.WriteLine($"Unable to deliver message. Reason: {ex.Message}");
+	}
+}
